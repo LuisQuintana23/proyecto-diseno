d39d50b7bf2232dea9ec469a54d42f4371b28577
LuisQuintana23 <luis10_2003@live.com.mx>
2022-03-10 22:11:40 -0600

    barra navegacion index.html arreglada

--- js/barra_responsive.js
+++ js/barra_responsive.js
@@ -1,10 +1,6 @@
-addEventListener('DOMContentLoaded', () =>{
-    const boton_menu = document.querySelector('.boton_menu')
-    if (boton_menu) {
-        boton_menu.addEventListener('click', () => {
-            const enlaces_barra = document.querySelector('.enlaces_navbar')
-            enlaces_barra.classList.toggle('show')
+const enlaces_barra = document.querySelector(".enlaces_navbar")
+const boton_barra = document.querySelector(".boton_barra")
 
-        })
-    }
+boton_barra.addEventListener("click", () => {
+    enlaces_barra.classList.toggle("barra_menu_visible")
 })
